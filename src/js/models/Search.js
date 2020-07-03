eefd44a104f9b646f14a1501195037c3bf9f0d2f
Caleb Buchta <calebbuchta97@gmail.com>
2020-07-03 17:18:55 -0500

    Initial commit

---
+++ src/js/models/Search.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
